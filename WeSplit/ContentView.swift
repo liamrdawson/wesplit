dde22c25003581bb8de8f468183e8b2dcfe03488
Liam Dawson <liamrdawson@gmail.com>
2020-05-18 22:40:56 +0100

    Create a form which utilises a two way binding with state, updating a text value based on the state value.

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,15 +9,17 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    @State var tapCount = 0
+    // @State is specifically designed for keeping track of simple properties in one view.
+    // It's recommended to provide private access control to those properties.
+    @State private var name = ""
     
     var body: some View {
-        Button("Tap Count \(tapCount)") {
-            self.tapCount += 1
+        Form {
+            // By adding a $ before name, we're creating a two way binding, this means that the text field reads the empty string in state and state reads and is updated by the value input to the TextField.
+            TextField("Enter your name:", text: $name)
+            Text("Your name is: \(name)")
         }
     }
-    
 }
 
 struct ContentView_Previews: PreviewProvider {
