59657b43d704a7bd345e4426e542eecc2da08932
Liam Dawson <liamrdawson@gmail.com>
2020-05-18 21:25:30 +0100

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Liam Dawson on 18/05/2020.
+//  Copyright © 2020 Liam Dawson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
